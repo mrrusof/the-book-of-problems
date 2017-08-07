1fcf0222f1ba14b6b591664db1cfaa84bbdfa349
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2017-08-06 22:27:34 -0500

    Alert for ruling of The Courtroom.

--- app/controllers/problems/show.js
+++ app/controllers/problems/show.js
@@ -29,26 +29,32 @@ end`,
     'vim'
   ],
 
-  judge: Ember.computed('store', function() {
-    return this.store.findRecord('judge', 'ruby');
-  }),
+  alertVisible: false,
+  alertType: 'alert-success',
+  ruling: '',
 
   actions: {
 
     selectJudge(id) {
         this.store.findRecord('judge', id).then(judge => {
-            this.set('judge', judge);
             this.set('mode', judge.get('codemirrormode'));
         });
     },
 
     submitCode() {
-        alert("Submitting the following code:\n\n" + this.code);
-        this.judge.trial({ src: this.code,
-                           problemId: this.model.id })
-            .then(response => {
-                alert("Response: " + JSON.stringify(response));
-            });
+      this.store.findRecord('judge', document.getElementById('judge').value).then(judge => {
+        judge.trial({ src: this.code,
+                      problemId: this.model.id })
+             .then(response => {
+                 if(response.ruling === "ACCEPTED") {
+                   this.set('alertType', 'success');
+                 } else {
+                   this.set('alertType', 'danger');
+                 }
+                 this.set('alertVisible', true);
+                 this.set('ruling', response.ruling + ", ran for " + response.seconds + " seconds.");
+             });
+      });
     }
 
   }
