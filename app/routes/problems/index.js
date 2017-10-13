f1899bfbdcdfe465978d907c74a8a02a982aee8e
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2017-10-13 01:01:40 -0500

    Show timestamps and sort by timestamps.

--- app/routes/problems/index.js
+++ app/routes/problems/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.get('store').findAll('problem');
+    return this.get('store').findAll('problem').then(pp => pp.sortBy('timestamp').reverse());
   }
 });
